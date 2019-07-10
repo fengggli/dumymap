b4924f0972330214c21869cd7c550fc9e7921566
Feng Li <fengggli.yahoo.com>
2019-07-10 17:18:11 +0000

    format

--- dummymap.c
+++ dummymap.c
@@ -1,29 +1,29 @@
-#include <linux/version.h>
-#include <linux/init.h>             // Macros used to mark up functions e.g., __init __exit
-#include <linux/module.h>           // Core header for loading LKMs into the kernel
-#include <linux/kernel.h>           // Contains types, macros, functions for the kernel
-#include <linux/errno.h>
-#include <linux/kernel.h>
-#include <linux/mm.h>
-#include <linux/types.h>
-#include <linux/miscdevice.h>
 #include <linux/compat.h>
-#include <linux/fs.h>
-#include <linux/slab.h>
-#include <linux/uaccess.h>
 #include <linux/dma-mapping.h>
+#include <linux/errno.h>
+#include <linux/fs.h>
+#include <linux/init.h> // Macros used to mark up functions e.g., __init __exit
+#include <linux/kernel.h> // Contains types, macros, functions for the kernel
+#include <linux/kernel.h>
+#include <linux/miscdevice.h>
+#include <linux/mm.h>
+#include <linux/module.h> // Core header for loading LKMs into the kernel
+#include <linux/slab.h>
+#include <linux/types.h>
+#include <linux/uaccess.h>
+#include <linux/version.h>
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,13,0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 13, 0)
 #include <asm/cacheflush.h>
 #else
 #include <asm/set_memory.h>
 #endif
 
 /* Linux 5 drops the mode atribute in access_ok */
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5,0,0)
-#define ACCESS_OK(mode,addr,size) access_ok(mode,addr,size)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 0, 0)
+#define ACCESS_OK(mode, addr, size) access_ok(mode, addr, size)
 #else
-#define ACCESS_OK(mode,addr,size) access_ok(addr,size)
+#define ACCESS_OK(mode, addr, size) access_ok(addr, size)
 #endif
 
 #include <asm/pgtable.h>
@@ -33,67 +33,62 @@
 
 // #include "dummymap.h"
 
-
-MODULE_LICENSE("GPL");              ///< The license type -- this affects runtime behavior
+MODULE_LICENSE("GPL");    ///< The license type -- this affects runtime behavior
 MODULE_AUTHOR("Feng Li"); ///< The author -- visible when you use modinfo
-MODULE_DESCRIPTION("dummy remap module.");  ///< The description -- see modinfo
-MODULE_VERSION("0.1");              ///< The version of the module
+MODULE_DESCRIPTION("dummy remap module."); ///< The description -- see modinfo
+MODULE_VERSION("0.1");                     ///< The version of the module
 
 static int fop_mmap(struct file *file, struct vm_area_struct *vma);
 
-static int dummymap_dev_release(struct inode *inode, struct file *file)
-{
+static int dummymap_dev_release(struct inode *inode, struct file *file) {
   pr_info("dummymap: release device");
   return 0;
 }
 
 static const struct file_operations dummymap_dev_ops = {
-  .owner          = THIS_MODULE,
-  .llseek         = noop_llseek,
-  .release        = dummymap_dev_release,
-  .mmap           = fop_mmap,
+    .owner = THIS_MODULE,
+    .llseek = noop_llseek,
+    .release = dummymap_dev_release,
+    .mmap = fop_mmap,
 };
 
 static struct miscdevice dummymap_dev = {
-  MISC_DYNAMIC_MINOR,
-  // XMS_MINOR,
-  "dummymap",
-  &dummymap_dev_ops,
+    MISC_DYNAMIC_MINOR,
+    // XMS_MINOR,
+    "dummymap",
+    &dummymap_dev_ops,
 };
 
-static void vm_open(struct vm_area_struct *vma)
-{
+static void vm_open(struct vm_area_struct *vma) {
   pr_info("dummymap: open vma");
 }
 
-static void vm_close(struct vm_area_struct *vma)
-{
+static void vm_close(struct vm_area_struct *vma) {
   pr_info("dummymap: close vma");
 }
 
 static struct vm_operations_struct dummymap_fops = {
-  .open   = vm_open,
-  .close  = vm_close,
+    .open = vm_open,
+    .close = vm_close,
 };
 
-
-static int fop_mmap(struct file *file, struct vm_area_struct *vma)
-{
+static int fop_mmap(struct file *file, struct vm_area_struct *vma) {
   size_t map_size;
-  void* dma_virtaddr;
+  void *dma_virtaddr;
   dma_addr_t dma_handle;
   map_size = vma->vm_end - vma->vm_start;
 
-  dma_virtaddr = dma_alloc_coherent(dummymap_dev.this_device, map_size, &dma_handle, GFP_KERNEL);
-  if(!dma_virtaddr){
+  dma_virtaddr = dma_alloc_coherent(dummymap_dev.this_device, map_size,
+                                    &dma_handle, GFP_KERNEL);
+  if (!dma_virtaddr) {
     return -ENOMEM;
   }
   vma->vm_page_prot = pgprot_noncached(vma->vm_page_prot);
 
-  if (remap_pfn_range(vma, vma->vm_start, virt_to_phys(dma_virtaddr)>>PAGE_SHIFT,
-                map_size,
-                vma->vm_page_prot))
-        return -ENOMEM;
+  if (remap_pfn_range(vma, vma->vm_start,
+                      virt_to_phys(dma_virtaddr) >> PAGE_SHIFT, map_size,
+                      vma->vm_page_prot))
+    return -ENOMEM;
 
   vma->vm_ops = &dummymap_fops;
   vm_open(vma);
@@ -104,27 +99,24 @@ static int __init dummymap_init(void) {
   int r;
 
   dummymap_dev.mode = S_IRUGO | S_IWUGO; // set permission for /dev/dummymap
-  
+
   r = misc_register(&dummymap_dev);
   if (r) {
     printk(KERN_ERR "dummymap: misc device register failed\n");
   }
   printk(KERN_INFO "dummymap: loaded\n");
-  
+
   return 0;
 }
 
-static void __exit dummymap_exit(void){
+static void __exit dummymap_exit(void) {
   printk(KERN_INFO "dummymap: unloaded\n");
   misc_deregister(&dummymap_dev);
 }
 
-/** @brief A module must use the module_init() module_exit() macros from linux/init.h, which
- *  identify the initialization function at insertion time and the cleanup function (as
- *  listed above)
+/** @brief A module must use the module_init() module_exit() macros from
+ * linux/init.h, which identify the initialization function at insertion time
+ * and the cleanup function (as listed above)
  */
 module_init(dummymap_init);
 module_exit(dummymap_exit);
- 
-
-
--- test-vfio.c
+++ test-vfio.c
@@ -1,25 +1,23 @@
-#include <linux/vfio.h>
-#include <sys/ioctl.h>
-#include <sys/ioctl.h>
-#include <sys/mman.h>
 #include <fcntl.h>
+#include <linux/vfio.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
 
+#include <assert.h>
 #include <errno.h>
 #include <string.h>
-#include <assert.h>
 
-#define PINF(f_, ...) printf((f_"\n"), ##__VA_ARGS__)
-#define PERR(f_, ...) fprintf(stderr, (f_"\n"), ##__VA_ARGS__)
-
+#define PINF(f_, ...) printf((f_ "\n"), ##__VA_ARGS__)
+#define PERR(f_, ...) fprintf(stderr, (f_ "\n"), ##__VA_ARGS__)
 
 #define VFIO_GROUP_PATH ("/dev/vfio/14")
 #define DEVICE_PCI ("0000:11:00.0")
 
 typedef enum {
   MEM_TYPE_ANON_REGULAR = 0, // anonymous mmap
-  MEM_TYPE_DUMMYMAP = 1, // filled by dummymap module (remap_pfn_range)
+  MEM_TYPE_DUMMYMAP = 1,     // filled by dummymap module (remap_pfn_range)
   MEM_TYPE_DEVDAX = 2,
 } mem_type_t;
 
@@ -27,163 +25,158 @@ typedef enum {
 /*static const mem_type_t mem_type = MEM_TYPE_ANON_REGULAR;*/
 static const mem_type_t k_mem_type = MEM_TYPE_DUMMYMAP;
 
+int run_register_dma(mem_type_t mem_type) {
+  int container, group, device, i;
+  struct vfio_group_status group_status = {.argsz = sizeof(group_status)};
+  struct vfio_iommu_type1_info iommu_info = {.argsz = sizeof(iommu_info)};
+  struct vfio_iommu_type1_dma_map dma_map = {.argsz = sizeof(dma_map)};
+  struct vfio_device_info device_info = {.argsz = sizeof(device_info)};
 
-int run_register_dma(mem_type_t mem_type){
-	int container, group, device, i;
-	struct vfio_group_status group_status =
-					{ .argsz = sizeof(group_status) };
-	struct vfio_iommu_type1_info iommu_info = { .argsz = sizeof(iommu_info) };
-	struct vfio_iommu_type1_dma_map dma_map = { .argsz = sizeof(dma_map) };
-	struct vfio_device_info device_info = { .argsz = sizeof(device_info) };
+  /* Create a new container */
+  container = open("/dev/vfio/vfio", O_RDWR);
 
-	/* Create a new container */
-	container = open("/dev/vfio/vfio", O_RDWR);
-
-	if (ioctl(container, VFIO_GET_API_VERSION) != VFIO_API_VERSION){
-		/* Unknown API version */
+  if (ioctl(container, VFIO_GET_API_VERSION) != VFIO_API_VERSION) {
+    /* Unknown API version */
     PERR("VFIO API unmatch");
     return -1;
   }
 
-	if (!ioctl(container, VFIO_CHECK_EXTENSION, VFIO_TYPE1_IOMMU)){
-		/* Doesn't support the IOMMU driver we want. */
+  if (!ioctl(container, VFIO_CHECK_EXTENSION, VFIO_TYPE1_IOMMU)) {
+    /* Doesn't support the IOMMU driver we want. */
     PERR("VFIO extension unmatch");
     return -1;
   }
 
-	/* Open the group */
-	group = open(VFIO_GROUP_PATH, O_RDWR);
-  if(group <0){
-    PERR("open group path failed at %s (change VFIO_GROUP_PATH)", VFIO_GROUP_PATH);
+  /* Open the group */
+  group = open(VFIO_GROUP_PATH, O_RDWR);
+  if (group < 0) {
+    PERR("open group path failed at %s (change VFIO_GROUP_PATH)",
+         VFIO_GROUP_PATH);
     return -1;
   }
 
-	/* Test the group is viable and available */
-	if(0 > ioctl(group, VFIO_GROUP_GET_STATUS, &group_status)){
+  /* Test the group is viable and available */
+  if (0 > ioctl(group, VFIO_GROUP_GET_STATUS, &group_status)) {
     PERR("ioctl failed with VFIO_GROUP_GET_STATUS");
     return -1;
   }
 
-	if (!(group_status.flags & VFIO_GROUP_FLAGS_VIABLE)){
-		/* Group is not viable (ie, not all devices bound for vfio) */
+  if (!(group_status.flags & VFIO_GROUP_FLAGS_VIABLE)) {
+    /* Group is not viable (ie, not all devices bound for vfio) */
     PERR("Group is not viable (ie, not all devices bound for vfio)");
     return -1;
   }
 
-	/* Add the group to the container */
-	if(0>ioctl(group, VFIO_GROUP_SET_CONTAINER, &container)){
-      PERR("Add the group to the container");
-      return -1;
+  /* Add the group to the container */
+  if (0 > ioctl(group, VFIO_GROUP_SET_CONTAINER, &container)) {
+    PERR("Add the group to the container");
+    return -1;
   };
 
-	/* Enable the IOMMU model we want */
-	ioctl(container, VFIO_SET_IOMMU, VFIO_TYPE1_IOMMU);
+  /* Enable the IOMMU model we want */
+  ioctl(container, VFIO_SET_IOMMU, VFIO_TYPE1_IOMMU);
 
-	/* Get addition IOMMU info */
-	ioctl(container, VFIO_IOMMU_GET_INFO, &iommu_info);
-  size_t map_size = 2*1024*1024; // 2MB
-  void * target_addr = ((void *) 0x900000000);
+  /* Get addition IOMMU info */
+  ioctl(container, VFIO_IOMMU_GET_INFO, &iommu_info);
+  size_t map_size = 2 * 1024 * 1024; // 2MB
+  void *target_addr = ((void *)0x900000000);
   int fd;
-  void * ptr;
+  void *ptr;
 
   /* Allocate some space and setup a DMA mapping */
-  switch(mem_type){
-    case MEM_TYPE_DEVDAX:
-      // https://github.com/axboe/fio/blob/master/engines/dev-dax.c
-      fd = open("/dev/dax0.0", O_RDWR, 0666);
-      assert(fd != -1);
-      ptr = mmap(NULL,
-                          map_size,
-                          PROT_READ | PROT_WRITE,
-                          // MAP_PRIVATE | MAP_FIXED, // | MAP_HUGETLB, // | MAP_HUGE_2MB,
-                          MAP_SHARED ,
-                          fd,
-                          0);
-      assert(ptr != MAP_FAILED);
-      // memset(ptr, 0xff, map_size);
+  switch (mem_type) {
+  case MEM_TYPE_DEVDAX:
+    // https://github.com/axboe/fio/blob/master/engines/dev-dax.c
+    fd = open("/dev/dax0.0", O_RDWR, 0666);
+    assert(fd != -1);
+    ptr = mmap(NULL, map_size, PROT_READ | PROT_WRITE,
+               // MAP_PRIVATE | MAP_FIXED, // | MAP_HUGETLB, // | MAP_HUGE_2MB,
+               MAP_SHARED, fd, 0);
+    assert(ptr != MAP_FAILED);
+    // memset(ptr, 0xff, map_size);
 
-      break;
+    break;
 
-    case MEM_TYPE_DUMMYMAP:
-      fd = open("/dev/dummymap", O_RDWR, 0666);
-      assert(fd != -1);
-      ptr = mmap(target_addr,
-                          map_size,
-                          PROT_READ | PROT_WRITE,
-                          // MAP_PRIVATE | MAP_FIXED, // | MAP_HUGETLB, // | MAP_HUGE_2MB,
-                          MAP_SHARED | MAP_FIXED ,
-                          fd,
-                          0);
-      assert(ptr != MAP_FAILED);
-      //memset(ptr, 0xff, map_size);
+  case MEM_TYPE_DUMMYMAP:
+    fd = open("/dev/dummymap", O_RDWR, 0666);
+    assert(fd != -1);
+    ptr = mmap(target_addr, map_size, PROT_READ | PROT_WRITE,
+               // MAP_PRIVATE | MAP_FIXED, // | MAP_HUGETLB, // | MAP_HUGE_2MB,
+               MAP_SHARED | MAP_FIXED, fd, 0);
+    assert(ptr != MAP_FAILED);
+    // memset(ptr, 0xff, map_size);
 
-      break;
-    case MEM_TYPE_ANON_REGULAR:
-    default:
-      dma_map.vaddr = mmap(target_addr, map_size, PROT_READ | PROT_WRITE,
-               MAP_PRIVATE | MAP_ANONYMOUS| MAP_FIXED, 0, 0);
+    break;
+  case MEM_TYPE_ANON_REGULAR:
+  default:
+    dma_map.vaddr = mmap(target_addr, map_size, PROT_READ | PROT_WRITE,
+                         MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, 0, 0);
   }
 
   dma_map.size = map_size;
   dma_map.iova = 0x900000000; /* 1MB starting at 0x900000000 from device view */
-	dma_map.flags = VFIO_DMA_MAP_FLAG_READ | VFIO_DMA_MAP_FLAG_WRITE;
+  dma_map.flags = VFIO_DMA_MAP_FLAG_READ | VFIO_DMA_MAP_FLAG_WRITE;
 
   PINF("press enter to map issue VFIO_IOMMU_MAP_DMA");
   getchar();
-	if(0> ioctl(container, VFIO_IOMMU_MAP_DMA, &dma_map)){
-      PERR("VFIO_IO_MMU_MAP_DMA failed with errno %s", strerror(errno));
-      return -1;
-  }
-  else{
+  if (0 > ioctl(container, VFIO_IOMMU_MAP_DMA, &dma_map)) {
+    PERR("VFIO_IO_MMU_MAP_DMA failed with errno %s", strerror(errno));
+    return -1;
+  } else {
     PINF("DMA memory setup succeed!");
   }
 
-	/* Get a file descriptor for the device */
-	device = ioctl(group, VFIO_GROUP_GET_DEVICE_FD, DEVICE_PCI);
-  if(device < 0){
-      PERR("IOCTRL with errno during get_device_fd (Change DEVICE_PCI): %s", strerror(errno));
-      return -1;
+  /* Get a file descriptor for the device */
+  device = ioctl(group, VFIO_GROUP_GET_DEVICE_FD, DEVICE_PCI);
+  if (device < 0) {
+    PERR("IOCTRL with errno during get_device_fd (Change DEVICE_PCI): %s",
+         strerror(errno));
+    return -1;
   }
 
-	/* Test and setup the device */
-	if(0 > ioctl(device, VFIO_DEVICE_GET_INFO, &device_info)){
+  /* Test and setup the device */
+  if (0 > ioctl(device, VFIO_DEVICE_GET_INFO, &device_info)) {
     PERR("IOCTRL with errno during get_info: %s", strerror(errno));
     return -1;
   }
 
-	for (i = 0; i < device_info.num_regions; i++) {
-		struct vfio_region_info reg = { .argsz = sizeof(reg) };
+  for (i = 0; i < device_info.num_regions; i++) {
+    struct vfio_region_info reg = {.argsz = sizeof(reg)};
 
-		reg.index = i;
+    reg.index = i;
 
-		ioctl(device, VFIO_DEVICE_GET_REGION_INFO, &reg);
+    ioctl(device, VFIO_DEVICE_GET_REGION_INFO, &reg);
 
-		/* Setup mappings... read/write offsets, mmaps
-		 * For PCI devices, config space is a region */
-	}
-
-	for (i = 0; i < device_info.num_irqs; i++) {
-		struct vfio_irq_info irq = { .argsz = sizeof(irq) };
-
-		irq.index = i;
-
-		ioctl(device, VFIO_DEVICE_GET_IRQ_INFO, &irq);
-
-		/* Setup IRQs... eventfds, VFIO_DEVICE_SET_IRQS */
-	}
-
-	/* Gratuitous device reset and go... */
-	ioctl(device, VFIO_DEVICE_RESET);
-}
-
-int main(int argc, char * argv[]){
-
-  int mem_type = MEM_TYPE_ANON_REGULAR;
-  if(argc>1 ){
-    mem_type = atoi(argv[1]);
-    if(mem_type > 2){ PERR("invalid memory type (1: dummymap, 2: devdax map)"); return -1;}
+    /* Setup mappings... read/write offsets, mmaps
+     * For PCI devices, config space is a region */
   }
 
-  PINF("Using memory type %d(0: anonymous mmap, 1: dummymap, 2: devdax map)", mem_type);
+  for (i = 0; i < device_info.num_irqs; i++) {
+    struct vfio_irq_info irq = {.argsz = sizeof(irq)};
+
+    irq.index = i;
+
+    ioctl(device, VFIO_DEVICE_GET_IRQ_INFO, &irq);
+
+    /* Setup IRQs... eventfds, VFIO_DEVICE_SET_IRQS */
+  }
+
+  /* Gratuitous device reset and go... */
+  ioctl(device, VFIO_DEVICE_RESET);
+}
+
+int main(int argc, char *argv[]) {
+
+  int mem_type = MEM_TYPE_ANON_REGULAR;
+  if (argc > 1) {
+    mem_type = atoi(argv[1]);
+    if (mem_type > 2) {
+      PERR("invalid memory type (1: dummymap, 2: devdax map)");
+      return -1;
+    }
+  }
+
+  PINF("Using memory type %d(0: anonymous mmap, 1: dummymap, 2: devdax map)",
+       mem_type);
   return run_register_dma(mem_type);
 }
